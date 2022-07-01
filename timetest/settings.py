1b6914da4833180936528cc9b7779979078a3fed
Talifhani Mulaudzi <mtalifhani54@gmail.com>
2022-07-01 10:47:56 +0200

    Configured the settings

--- timetest/settings.py
+++ timetest/settings.py
@@ -118,9 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = ''
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static', 
 ]
+
+MEDIA_ROOT = BASE_DIR / 'static/img'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
